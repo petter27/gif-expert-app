0cf9a4429d3ab8e3791f77612efe3dd8dbf5b501
Pedro Pacheco <pedropacheco1523503@gmail.com>
2022-07-14 22:37:36 -0600

    Creando build de produccion

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -6,6 +6,7 @@ export const GifGrid = ({ category }) => {
   // si un un hook useEffect las dependecias se dejan vacias solo se disparara una vez
   useEffect( () => {
     getGifs(category);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
 
   return (
