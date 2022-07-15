e2babdac3701baec40db431131ddfda41dc259ab
Pedro Pacheco <pedropacheco1523503@gmail.com>
2022-07-14 22:08:33 -0600

    Creando peticion fetch a API

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,9 @@
+import { getGifs } from '../helpers/getGifs'
 
 export const GifGrid = ({ category }) => {
+
+  getGifs(category);
+
   return (
     <>
       <h3>{ category }</h3>
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,17 @@
+export const getGifs = async(category) => {
+
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=zdkwju4zAgmjF5gGrI0fTYRz35mGN8jA&q=${category}&limit=20`;
+  const resp = await fetch( url );
+  const { data } = await resp.json();
+
+  const gifs = data.map( item => {
+    return {
+      id: item.id,
+      title: item.title,
+      url: item.images?.downsized_medium.url 
+    }
+    
+  })
+  console.log(gifs);
+  return gifs;
+}
